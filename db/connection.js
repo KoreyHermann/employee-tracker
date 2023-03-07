2b457bac6c920e590b2ae950d3a505c440f577a7
Korey Hermann <khermann135@gmail.com>
2023-03-07 13:17:40 -0500

    added sql files

---
+++ db/connection.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '', //add password here later
+    database: 'employee_tracker_db',
+
+});
+
+module.exports = db;
---
+++ index.js
@@ -0,0 +1,15 @@
+// sales - 1
+// engineering - 2
+// finance - 3
+// legal - 4
+
+// sales lead 100k
+// salesperson 80k
+// lead engineer 150k
+// software engineer 120k
+// account manager 160k
+// accountant 125k
+// legal team lead 250k
+// lawyer 190k
+
+// add password to db.sql later
